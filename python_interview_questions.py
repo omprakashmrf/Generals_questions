93bdc88df64930445a361f8db11b0a2f904c1f79
Omprakash <99076530+omprakashmrf@users.noreply.github.com>
2025-05-16 15:51:24 +0530

    Update python_interview_questions.py

--- python_interview_questions.py
+++ python_interview_questions.py
@@ -124,4 +124,18 @@ d=checking_account(2000)
 print(d.checking_acount())
 
 # print(str("asdsfdsf"))
+
+
+def is_prime(n):
+    if n < 2:
+        return False
+    for i in range(2, int(n ** 0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+for num in range(1, 101):
+    if is_prime(num):
+        print(num, end=' ')
+
         
