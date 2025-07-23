1f0470c4f0df521e92a614c15d21889daacae5f8
Omprakash <99076530+omprakashmrf@users.noreply.github.com>
2025-07-23 08:27:57 +0530

    Update Ds_algo.py

--- Ds_algo.py
+++ Ds_algo.py
@@ -1,1 +1,32 @@
 sliding window ---> problems
+
+# # brute force approch 
+def maxsumsubarr(arr, k):
+    max_sum = 0
+    for i in range(len(arr)-k +1):
+        current_sum = sum(arr[i: i+k])
+        max_sum = max(max_sum, current_sum)
+        
+    return max_sum
+arr = [2, 1, 5, 1, 3, 2]
+k = 3
+print(maxsumsubarr(arr, k))
+
+# optimal solution
+
+def max_sub_arr_sum(arr, k):
+    if len(arr) < k:
+        return None # not enough element for count
+    
+    window_sum = sum(arr[:k])
+    max_sum = window_sum
+    
+    for i in range(k, len(arr)):
+        window_sum += arr[i] - arr[i-k]
+        max_sum = max(max_sum, window_sum)
+    
+    return max_sum    
+        
+arr = [2, 1, 5, 1, 3, 2]
+k = 3
+print(max_sub_arr_sum(arr, k)) 
