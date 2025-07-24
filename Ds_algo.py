5867dbcfb665754a1543d88511cb6691f63ebdbb
Omprakash <99076530+omprakashmrf@users.noreply.github.com>
2025-07-24 15:35:23 +0530

    Update Ds_algo.py

--- Ds_algo.py
+++ Ds_algo.py
@@ -30,3 +30,48 @@ def max_sub_arr_sum(arr, k):
 arr = [2, 1, 5, 1, 3, 2]
 k = 3
 print(max_sub_arr_sum(arr, k)) 
+
+
+def longest_k_size(s, k):
+    if len(s)< k:
+        return 0
+    char_map = {}
+    max_len = 0
+    left =0
+    
+    for right in range(len(s)):
+        char=s[right]
+        
+        char_map[char]=char_map.get(char, 0)+1
+        
+        while len(char_map) >k:
+            left_char=s[left]
+            
+            char_map[left_char] -=1
+            if char_map[left_char]==0:
+                del char_map[left_char]
+            left +=1    
+            
+        max_len=max(max_len, right -left +1)    
+        
+    return max_len   
+s="eceba"
+k = 2        
+print(longest_k_size(s, k))  
+
+
+def longest_k_size_brute(s, k):
+    max_len = 0
+    for i in range(len(s)):
+        char_set = set()
+        for j in range(i, len(s)):
+            char_set.add(s[j])
+            if len(char_set) <= k:
+                max_len = max(max_len, j - i+1)
+            else:
+                break;
+    return max_len    
+s="eceba"
+k = 2      
+print(longest_k_size_brute(s, k))    
+    
