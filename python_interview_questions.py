ed9fe6d2091652a6a11584bf711b22de786714b2
Omprakash <99076530+omprakashmrf@users.noreply.github.com>
2026-04-02 10:46:29 +0530

    Update python_interview_questions.py

--- python_interview_questions.py
+++ python_interview_questions.py
@@ -1362,5 +1362,14 @@ def poilindrom(s):
 
 poilindrom("i raR I") 
 
+def next_power_of_2(n):
+    if n == 0:
+        return 1
+    
+    power = 1
+    while power <= n:
+        power <<= 1   # multiply by 2
+    return power    
 
+print(next_power_of_2(128))
  
