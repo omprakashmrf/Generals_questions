c36f5a5f998cf697d9b54205fa1f5aaa367da962
Omprakash <99076530+omprakashmrf@users.noreply.github.com>
2026-09-04 15:36:34 +0530

    Implement longest substring without repeating characters
    Added a function to find the length of the longest substring without repeating characters, along with example inputs and outputs.

--- python_code_first_round_interview.py
+++ python_code_first_round_interview.py
@@ -946,4 +946,35 @@ class Solution:
 obj= Solution()
 s= "my name is omprakash"
 print(obj.reverseWords(s))
-    
+
+# Given a string s, find the length of the longest substring without repeating characters.
+# Example 1:
+# Input: s = "abcdxyzxb"
+# Output: 7
+# Explanation: The answer is "abcdxyz", with the length of 7.
+# Example 2:
+# Input: s = "abcdabcbbxyzt"
+# Output: 5
+# Explanation: The answer is "bxyzt", with the length of 5.
+# Example 3:
+# Input: s = "pwwkew"
+# Output: 3
+# Explanation: The answer is "wke", with the length of 3.
+# Notice that the answer must be a substring, "pwke" is a subsequence and not a subs
+#tring.
+
+def longest_substring(s):
+    longest = 0
+    char_index ={}
+    left = 0
+
+    for right, ch in enumerate(s):
+        if ch in char_index and char_index[ch] >=left:
+            left = char_index[ch] + 1
+        char_index[ch] = right
+
+        longest = max(longest, right - left +1)
+    return longest 
+
+s = "abcdabcbbxyzt"
+print(longest_substring(s))
